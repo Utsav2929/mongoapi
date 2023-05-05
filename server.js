97964264f3c21f1dc8c34b9444603275f93ed84f
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-05-05 11:29:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ import { fetchData } from "./api/fetch.js";
 
 const port = process.env.PORT || 5000;
 
+const server = http.createServer(app);
+server.listen(port,()=>{
+
 setInterval(() => {
     let obj = [];
     fetchData("user").then((data) => {
       obj = data;
   
       for (let x of obj) {
-        // console.log(x);
+        console.log(x);
         if (x.allowed === true) {
           const insertUser = async () => {
             const check = await user.findOne({ email: x.email });
@@ -21,21 +24,18 @@ setInterval(() => {
                 password: x.password,
                 name: `${x.firstName} ${x.lastName}`,
               });
-              // console.log(res);
+               console.log(res);
             }
           };
           insertUser();
         } else {
           const deleteUser = async () => {
             const res = await user.deleteMany({ email: x.email });
-            // console.log(res);
+             console.log(res);
           };
           deleteUser();
         }
       }
     });
   }, 10000);
-const server = http.createServer(app);
-server.listen(port,()=>{
-
     console.log('this app is running on '+port)});
