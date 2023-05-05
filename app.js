08359b0021456eb7c78512f97954f84662bd46b4
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-05-05 11:38:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,36 +22,36 @@ app.use('/user',userRoutes);
 // PORT=5000
 // CONNECTION_URL=mongodb+srv://sih2022:sih2022@societymanagment.teylon6.mongodb.net/?retryWrites=true&w=majority
 
-app.use('/',setInterval(() => {
-    let obj = [];
-    fetchData("user").then((data) => {
-      obj = data;
+// app.use('/',setInterval(() => {
+//     let obj = [];
+//     fetchData("user").then((data) => {
+//       obj = data;
   
-      for (let x of obj) {
-        console.log(x);
-        if (x.allowed === true) {
-          const insertUser = async () => {
-            const check = await user.findOne({ email: x.email });
-            if (!check) {
-              const res = await user.create({
-                email: x.email,
-                password: x.password,
-                name: `${x.firstName} ${x.lastName}`,
-              });
-               console.log(res);
-            }
-          };
-          insertUser();
-        } else {
-          const deleteUser = async () => {
-            const res = await user.deleteMany({ email: x.email });
-             console.log(res);
-          };
-          deleteUser();
-        }
-      }
-    });
-  }, 10000))
+//       for (let x of obj) {
+//         console.log(x);
+//         if (x.allowed === true) {
+//           const insertUser = async () => {
+//             const check = await user.findOne({ email: x.email });
+//             if (!check) {
+//               const res = await user.create({
+//                 email: x.email,
+//                 password: x.password,
+//                 name: `${x.firstName} ${x.lastName}`,
+//               });
+//                console.log(res);
+//             }
+//           };
+//           insertUser();
+//         } else {
+//           const deleteUser = async () => {
+//             const res = await user.deleteMany({ email: x.email });
+//              console.log(res);
+//           };
+//           deleteUser();
+//         }
+//       }
+//     });
+//   }, 10000))
 const PORT =process.env.PORT;
 // // mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
 // .then(()=>app.listen(PORT,()=>console.log(`server running on port ${PORT}`))).catch((error)=>console.log(`${error} did not connected`));
