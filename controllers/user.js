26f55d61457b99126f23a6a38fe84664afda8929
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-05-06 21:06:25 +0530

    added

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import user from '../models/user.js';
 
 import { fetchData } from "../api/fetch.js";
 
