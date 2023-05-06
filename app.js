c3bdd20928e558cff632e751ddbb408a37e75208
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-05-06 13:36:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import user from './models/user.js';
 
 import { fetchData } from "./api/fetch.js";
+
 import userRoutes from './routes/users.js';
 
 const app = express();
@@ -22,36 +23,6 @@ app.use('/user',userRoutes);
 // PORT=5000
 // CONNECTION_URL=mongodb+srv://sih2022:sih2022@societymanagment.teylon6.mongodb.net/?retryWrites=true&w=majority
 
-// app.use('/',setInterval(() => {
-//     let obj = [];
-//     fetchData("user").then((data) => {
-//       obj = data;
-  
-//       for (let x of obj) {
-//         console.log(x);
-//         if (x.allowed === true) {
-//           const insertUser = async () => {
-//             const check = await user.findOne({ email: x.email });
-//             if (!check) {
-//               const res = await user.create({
-//                 email: x.email,
-//                 password: x.password,
-//                 name: `${x.firstName} ${x.lastName}`,
-//               });
-//                console.log(res);
-//             }
-//           };
-//           insertUser();
-//         } else {
-//           const deleteUser = async () => {
-//             const res = await user.deleteMany({ email: x.email });
-//              console.log(res);
-//           };
-//           deleteUser();
-//         }
-//       }
-//     });
-//   }, 10000))
 const PORT =process.env.PORT;
 // // mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
 // .then(()=>app.listen(PORT,()=>console.log(`server running on port ${PORT}`))).catch((error)=>console.log(`${error} did not connected`));
@@ -66,12 +37,39 @@ mongoose.connection.on('error',err=>{
 mongoose.connection.on('connected',()=>{
   console.log('connected successfully with database');
 });
-
+setInterval(() => {
+  let obj = [];
+  fetchData("user").then((data) => {
+    obj = data;
+    for (let x of obj) {
+      
+      if (x.allowed === true) {
+        const insertUser = async () => {
+          const check = await user.findOne({ email: x.email });
+          if (!check) {
+            const res = await user.create({
+              email: x.email,
+              password: x.password,
+              name: `${x.firstName} ${x.lastName}`,
+            });
+            
+          }
+        };
+        insertUser();
+      }
+       else {
+        const deleteUser = async () => {
+          const res = await user.deleteMany({ email: x.email });
+         
+        };
+        deleteUser();
+       }
+    }
+  });
+}, 10000);
 app.get('*',(req,res)=>{
-  
-
     res.status(200).json({
-      message:'server running'
+      message:'bad request'
     })
   })
  export default app;
